5dd19b6a6eaacb8fec9d25af4cbfd3eb9509175d
Lis2007K <lis2kastrati@gmail.com>
2025-12-07 23:35:34 +0100

    Make delivery options interactive

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -52,3 +52,17 @@ export function removeFromCart(productId) {
 
     saveToStorage();
 };
+
+export function updateDeliveryOtion(productId, deliveryOptionId){
+  let matchingItem;
+
+    cart.forEach((cartItem) => {
+      if(productId === cartItem.productId){
+          matchingItem = cartItem;
+      }
+    });
+
+    matchingItem.deliveryOptionId = deliveryOptionId;
+
+    saveToStorage();
+}
